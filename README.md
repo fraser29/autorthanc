15924965cec078fdd6c06a7ee2a94f98bd8bd4f2
Fraser Callaghan <fraser.callaghan@kispi.uzh.ch>
2023-12-11 11:38:46 +0100

    further dev of automation

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ Docker based infrastructure for OHIF Viewer, Orthanc (with postgres)
 
 ## DEVELOPMENT 
 
-Idea is that have standard osimis orthanc docker.
-  have a mounted volume at /var/lib/automation
-Add a python that on every study stable - read /var/lib/automation/master.json 
-  get list of study_stable instructions
-    if study_stable_instruction_XXX.json is present then check if meets conditions. If so then run. 
-  repeat: series_stable
+Idea is that have standard osimis orthanc docker, with a mounted volume at /var/lib/automation
 
-  Also in master.json have option to run do all instructions check daily at hour XX (every hour run to check this).
+Add a python script that on every study stable - read /var/lib/automation/master.json 
+
+ - Get list of study_stable instructions
+ - if study_stable_instruction_XXX.json is present then check if meets conditions. If so then run. 
+   - possible to repeat on series_stable (but suggest not to do this). 
+
+Also in master.json have option to run do all instructions check daily at hour XX (every hour run to check this).
 
 .env file defines local directory for automation jsons. This local directory is mounted at /automation in the docker container.
 
@@ -21,11 +22,13 @@ A study_stable or series_stable task may do any/all of the  following steps:
 - Anonymise  dicoms
 - ZIP data. 
 
+*NOTE* : package [spydcmtk](https://pypi.org/project/spydcmtk/) is used for dicom study organisation
+
 *NOTE* : package [miresearch](https://github.com/fraser29/miresearch) is designed to work in combination with autorthanc by watching docnload directorys for completed data sets to then perform extra post processing steps, e.g. to initiate processing pipelines. 
 
 
 
-=======
+--------------------
 
 Docker images :
 - osimis/orthanc:20.2.0
@@ -34,7 +37,7 @@ Docker images :
 To deploy the infrastructure, run `docker compose up` or `docker compose up -d` (background task).
 
 ## Access OHIF Viewer
-A few seconds after deploying the images, the viewer can be accessed at [http://127.0.0.1:81/](http://127.0.0.1:81/) from your navigator.
+A few seconds after deploying the images, the viewer can be accessed at [http://127.0.0.1:81/](http://127.0.0.1:81/) from your browser.
 
 ## Access Orthanc admin interface
 
@@ -62,7 +65,7 @@ rm -rf orthanc_db pg_data
 
 ## Python script action:
 
-- Triggered on SeriesStable and StudyStable 
+- Triggered on StudyStable (and SeriesStable possible - but not used) 
 - Recognises a series / study based upon rules defined in json script 
 - Performs actions 
   - First to DOWNLOADING directory
--- orthanc/Automation.py
+++ orthanc/Automation.py
@@ -13,22 +13,37 @@ import json
 import pydicom
 import io
 import shutil
-
-# TODO move to config
-SERIES_CONTAINS_STR = ''
-outputDir = '/output'
-DOWNLOADING = 'DOWNLOADING'
-QUEUED = 'QUEUED'
-USERNAME = 1000
-GROUPNAME = 1000
+import logging
 
 
+# ============ CONFIG ==============================================================================
+auto_scripts="/auto_scripts"
+output="/output"
+DOWNLOAD_DIR = os.path.join(output, 'DOWNLOADING')
+QUEUED_DIR = os.path.join(output, 'QUEUED')
+USERID = 1000
+GROUPID = 1000
+logfile = os.path.join(auto_scripts, 'orthanc_automation.log')
+
+# ============ LOGGING =============================================================================
+logger = logging.getLogger(f"autorthanc")
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler(logfile, encoding='utf-8')
+fh.setFormatter(logging.Formatter('%(asctime)s | %(levelname)-7s | %(name)s | %(message)s', 
+                                  datefmt='%d-%b-%y %H:%M:%S'))
+logger.addHandler(fh)
+logging.info('Autorthanc automation initiated.')
+
+
+# -------------------------------------------------------------------------------------------------
+def initStorage():
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    os.makedirs(QUEUED_DIR, exist_ok=True)
 
 def writeDictionaryToJSON(fileName, dictToWrite):
     with open(fileName, 'w') as fp:
         json.dump(dictToWrite, fp, indent=4, sort_keys=True)
     return fileName
-
 
 def parseJsonToDictionary(fileName):
     with open(fileName, 'r') as fid:
@@ -37,55 +52,125 @@ def parseJsonToDictionary(fileName):
 
 def readMasterJSON(masterFile):
     masterD = parseJsonToDictionary(masterFile)
-    
+
+def getAllAutomationDictionary():
+    autoscripts = [os.path.join(auto_scripts, i) for i in os.listdir(auto_scripts) if \
+                   (i.endswith('json') and (not i.startswith('master')) and (not 'template' in i))]
+    autoDicts = []
+    for iFile in autoscripts:
+        try:
+            iDict = parseJsonToDictionary(iFile)
+            if iDict.get("IsActive", False):
+                iDict['ID'] = os.path.splitext(os.path.split(iFile)[1])[0]
+                autoDicts.append(iDict)
+        except json.decoder.JSONDecodeError:
+            logger.error(f"{iFile} has incorrect format")
+        except:
+            logger.error(f"Error reading {iFile}")
+    return autoDicts
+
+def doesStudyMatchAutoDict(studyID, autoDict):
+    metaPatient = json.loads(orthanc.RestApiGet(f'/studies/{studyID}'))
+    metaStudy = json.loads(orthanc.RestApiGet(f'/studies/{studyID}'))
+    metaSeriesList = [json.loads(orthanc.RestApiGet(f'/series/{iSeries}')) for iSeries in metaStudy['Series']]
+    allTF = []
+    for iTag in autoDict['Tags']:
+        if iTag['Level'].lower() == 'patient':
+            thisValue = metaPatient['MainDicomTags'][iTag['TagName']].lower()
+            allTF.append(iTag['Value'].lower() in thisValue)
+        elif iTag['Level'].lower() == 'study':
+            thisValue = metaStudy['MainDicomTags'][iTag['TagName']].lower()
+            allTF.append(iTag['Value'].lower() in thisValue)
+        elif iTag['Level'].lower() == 'series':
+            thisValues = [i['MainDicomTags'][iTag['TagName']] for i in metaSeriesList]
+            tf = [iTag['Value'].lower() in i for i in thisValues]
+            allTF.append(any(tf))
+    return all(allTF)
 
 
-def initStorage():
-    os.makedirs(f'{outputDir}/{DOWNLOADING}', exist_ok=True)
-    os.makedirs(f'{outputDir}/{QUEUED}', exist_ok=True)
+def checkAutomationScriptsForStudy(studyID):
+    allAutoScripts = getAllAutomationDictionary()
+    resDicts = []
+    for iAuto in allAutoScripts:
+        if doesStudyMatchAutoDict(studyID, iAuto):
+            resDicts.append(iAuto)
+    return resDicts
+# ------------------------------------------------------------------------------------------------
 
-def getInstances(seriesID):
+
+def getInstancesSeries(seriesID):
     allInstanceIds = json.loads(orthanc.RestApiGet(f"/series/{seriesID}/instances"))
     return allInstanceIds
 
-def writeOutSeriesIDToFile(seriesID): # For testing
-    with open(f'{outputDir}/uro_out.txt', 'w') as fid:
-        fid.write(seriesID)
+def getSeriesStudy(studyID):
+    return json.loads(orthanc.RestApiGet(f"/studies/{studyID}"))['Series']
 
-def getOutputDir(seriesID, DOWNLOAD=False):
-    metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
-    metaPat = json.loads(orthanc.RestApiGet(f'/studies/{metadata["ParentStudy"]}'))
-    prefix = f'{DOWNLOADING}/' if DOWNLOAD else f'{QUEUED}/'
+def getInstancesStudy(studyID):
+    instanceIDs = []
+    for iSeries in getSeriesStudy(studyID):
+        instanceIDs += getInstancesSeries(iSeries)
+    return instanceIDs
+
+# def writeOutSeriesIDToFile(seriesID): # For testing
+#     with open(f'{config["ORTHANC_AUTOMATION_DATA"]}/uro_out.txt', 'w') as fid:
+#         fid.write(seriesID)
+
+def getInstanceSaveFile(instanceID, rootDir):
+    metadata = json.loads(orthanc.RestApiGet(f'/instances/{instanceID}'))
+    metaSer = json.loads(orthanc.RestApiGet(f'/series/{metadata["ParentSeries"]}'))
+    seNum = metaSer["MainDicomTags"]["SeriesNumber"]
+    seDate = metaSer["MainDicomTags"]["SeriesDate"]
+    seUID = metaSer["MainDicomTags"]["SeriesInstanceUID"]
+    return os.path.join(rootDir, f'SE{seNum}-{seDate}-{seUID}', f'{metadata["MainDicomTags"]["SOPInstanceUID"]}.dcm')
+
+# def getDownloadDirSeries(seriesID):
+#     metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
+#     metaPat = json.loads(orthanc.RestApiGet(f'/studies/{metadata["ParentStudy"]}'))
+#     name = metaPat["PatientMainDicomTags"]["PatientName"].split('^')[0]
+#     patientDir = os.path.join(DOWNLOAD_DIR, f'{metaPat["PatientMainDicomTags"]["PatientID"]}-{name}')
+#     seriesOutDir = f'{patientDir}-SE{metadata["MainDicomTags"]["SeriesNumber"]}-{metadata["MainDicomTags"]["SeriesDate"]}-{metadata["MainDicomTags"]["SeriesInstanceUID"]}'
+#     return seriesOutDir
+
+def getDownloadDirStudy(studyID, prefix=''):
+    metaPat = json.loads(orthanc.RestApiGet(f'/studies/{studyID}'))
     name = metaPat["PatientMainDicomTags"]["PatientName"].split('^')[0]
-    patientDir = f'{outputDir}/{prefix}{metaPat["PatientMainDicomTags"]["PatientID"]}-{name}'
-    seriesOutDir = f'{patientDir}-SE{metadata["MainDicomTags"]["SeriesNumber"]}-{metadata["MainDicomTags"]["SeriesDate"]}-{metadata["MainDicomTags"]["SeriesInstanceUID"]}'
-    return seriesOutDir
+    prefix_ = prefix+'_' if len(prefix) > 0 else prefix
+    patientDir = os.path.join(DOWNLOAD_DIR, f'{prefix_}{metaPat["PatientMainDicomTags"]["PatientID"]}-{name}')
+    return patientDir
+
+def getQueuedDir(studyID):
+    downloadDir = getDownloadDirStudy(studyID)
+    return downloadDir.replace(DOWNLOAD_DIR, QUEUED_DIR)
 
 def changeOwnership(directory, userName, groupName):
     os.system(f"chown -R {userName}:{groupName} {directory}")
 
-def writeOutSeriesToDirectory(seriesID, FORCE=False):
-    queuedDIR = getOutputDir(seriesID, DOWNLOAD=False) # QUEUED
+def writeOutStudyToDirectory(studyID, prefix='', FORCE=False):
+    downloadDIR = getDownloadDirStudy(studyID, prefix) # Downloading
+    queuedDIR = getQueuedDir(studyID)
     if os.path.isdir(queuedDIR):
         if FORCE:
+            logger.warning(f"Removing {queuedDIR} to rewrite {studyID}")
             shutil.rmtree(queuedDIR)
         else:
+            logger.warning(f"{studyID} already exists - not written out")
             return queuedDIR
-    instances = getInstances(seriesID)
-    downloadDIR = getOutputDir(seriesID, DOWNLOAD=True) # DOWNLOADING
+    logger.info(f"Begin writing out study {studyID}")
+    instances = getInstancesStudy(studyID)
     os.makedirs(downloadDIR, exist_ok=True)
     for instanceId in instances:
+        instanceSaveFile = getInstanceSaveFile(instanceId['ID'], downloadDIR)
+        os.makedirs(os.path.split(instanceSaveFile)[0], exist_ok=True)
         dicom = instanceToPyDicom(instanceId['ID'])
-        dicom.save_as(f'{downloadDIR}/{dicom.SOPInstanceUID}.dcm', write_like_original=True)
+        dicom.save_as(instanceSaveFile, write_like_original=True)
+        #
+    logger.info(f"Finished writting {studyID}")
     os.rename(downloadDIR, queuedDIR)
-    changeOwnership(queuedDIR, USERNAME, GROUPNAME)
+    logger.info(f"Moved {downloadDIR} to {queuedDIR}")
+    changeOwnership(queuedDIR, USERID, GROUPID)
+    logger.info(f"Set ownership of {queuedDIR} to {USERID}:{GROUPID}")
+    logger.info(f"WRITTEN: {queuedDIR}")
     return queuedDIR
-
-def confirmSeriesWrittenOut(seriesID, destinationDir):
-    metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
-    nInstances = len(metadata['Instances'])
-    print(f'DEBUG: Expect {nInstances} instances -> list dir count = {len(os.listdir(destinationDir))}')
-    return len(os.listdir(destinationDir)) == nInstances
 
 def instanceToPyDicom(instanceID):
     f = orthanc.GetDicomForInstance(instanceID)
@@ -93,88 +178,123 @@ def instanceToPyDicom(instanceID):
     dicom = pydicom.dcmread(io.BytesIO(f))
     return dicom
 
-def isSeriesDescription(seriesID, matchStr):
-    metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
-    try:
-        seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
-    except KeyError:
-        return False
-    return matchStr.lower() in seriesDescription.lower()
 
-def isUrography(seriesID):
-    metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
-    try:
-        seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
-        seriesNumber = int(metadata["MainDicomTags"]["SeriesNumber"])
-    except KeyError:
-        return False
-    return seriesDescription.endswith("(time)") and (seriesNumber < 100)
+# def confirmSeriesWrittenOut(seriesID, destinationDir):
+#     metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
+#     nInstances = len(metadata['Instances'])
+#     logger.info(f'COUNT: Expect {nInstances} instances -> list dir count = {len(os.listdir(destinationDir))}')
+#     return len(os.listdir(destinationDir)) == nInstances
 
-def isUrographyPDF(seriesID):
-    metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
-    try:
-        seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
-        seriesNumber = int(metadata["MainDicomTags"]["SeriesNumber"])
-    except KeyError:
-        return False
-    return seriesDescription.endswith("ResultsPDF")
+# def isSeriesDescription(seriesID, matchStr):
+#     metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
+#     try:
+#         seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
+#     except KeyError:
+#         return False
+#     return matchStr.lower() in seriesDescription.lower()
+
+# def isUrography(seriesID):
+#     metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
+#     try:
+#         seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
+#         seriesNumber = int(metadata["MainDicomTags"]["SeriesNumber"])
+#     except KeyError:
+#         return False
+#     return seriesDescription.endswith("(time)") and (seriesNumber < 100)
+
+# def isUrographyPDF(seriesID):
+#     metadata = json.loads(orthanc.RestApiGet(f'/series/{seriesID}'))
+#     try:
+#         seriesDescription = metadata["MainDicomTags"]["SeriesDescription"]
+#         seriesNumber = int(metadata["MainDicomTags"]["SeriesNumber"])
+#     except KeyError:
+#         return False
+#     return seriesDescription.endswith("ResultsPDF")
 
 # ================================================================================================================
-def AutoPipelineOnStableSeries(seriesID, FORCE=False):
-    # TODO read master.json and get all ID  and action stuff
-    # tf = isSeriesDescription(seriesID, SERIES_CONTAINS_STR)
-    tf = isUrography(seriesID)
-    if tf:
-        print(f'Have new series {seriesID}. Is UROGRAPHY: {tf}')
-        outputDir = writeOutSeriesToDirectory(seriesID, FORCE)
-        if confirmSeriesWrittenOut(seriesID, outputDir): # Checks file count vs count in Orthanc
-            orthanc.RestApiDelete(f'/series/{seriesID}')
-            return 0
-        else:
-            # In this case - hopefully get some more dicoms and then force the IS_STABLE flag again
-            # Or at least not delete and can manually write (in case there was a writting problem)
-            return 1
-    tf2 = isUrographyPDF(seriesID)
-    if tf2:
-        print(f'Have new series {seriesID}. Is UROGRAPHYPDF: {tf2}')
-        orthanc.RestApiPost('/modalities/PACS/store', seriesID)
-        # orthanc.RestApiDelete(f'/series/{seriesID}')
-        # return 0
-    return 10 # if is not URO - still success
+# def AutoPipelineOnStableSeries(seriesID, FORCE=False):
+#     # TODO read master.json and get all ID  and action stuff
+#     # tf = isSeriesDescription(seriesID, SERIES_CONTAINS_STR)
+#     tf = isUrography(seriesID)
+#     if tf:
+#         print(f'Have new series {seriesID}. Is UROGRAPHY: {tf}')
+#         outputDir = writeOutSeriesToDirectory(seriesID, FORCE)
+#         if confirmSeriesWrittenOut(seriesID, outputDir): # Checks file count vs count in Orthanc
+#             orthanc.RestApiDelete(f'/series/{seriesID}')
+#             return 0
+#         else:
+#             # In this case - hopefully get some more dicoms and then force the IS_STABLE flag again
+#             # Or at least not delete and can manually write (in case there was a writting problem)
+#             return 1
+#     tf2 = isUrographyPDF(seriesID)
+#     if tf2:
+#         print(f'Have new series {seriesID}. Is UROGRAPHYPDF: {tf2}')
+#         orthanc.RestApiPost('/modalities/PACS/store', seriesID)
+#         # orthanc.RestApiDelete(f'/series/{seriesID}')
+#         # return 0
+#     return 10 # if is not URO - still success
+
+def AutoPipelineOnStableStudy(studyID, FORCE=False):
+    resDicts = checkAutomationScriptsForStudy(studyID)
+    for iResDict in resDicts:
+        writeOutStudyToDirectory(studyID, iResDict['ID'], FORCE=FORCE)
+    # if tf:
+    #     print(f'Have new series {seriesID}. Is UROGRAPHY: {tf}')
+    #     outputDir = writeOutSeriesToDirectory(seriesID, FORCE)
+    #     if confirmSeriesWrittenOut(seriesID, outputDir): # Checks file count vs count in Orthanc
+    #         orthanc.RestApiDelete(f'/series/{seriesID}')
+    #         return 0
+    #     else:
+    #         # In this case - hopefully get some more dicoms and then force the IS_STABLE flag again
+    #         # Or at least not delete and can manually write (in case there was a writting problem)
+    #         return 1
+    # tf2 = isUrographyPDF(seriesID)
+    # if tf2:
+    #     print(f'Have new series {seriesID}. Is UROGRAPHYPDF: {tf2}')
+    #     orthanc.RestApiPost('/modalities/PACS/store', seriesID)
+    #     # orthanc.RestApiDelete(f'/series/{seriesID}')
+    #     # return 0
+    return 0 # if is not URO - still success
             
 # ================================================================================================================
 
 
-def ManualCallURO(output, uri, **request):
-    if request['method'] == 'GET':
-        # Retrieve the series ID from the regular expression (*)
-        seriesId = request['groups'][0]
-        result = AutoPipelineOnStableSeries(seriesId, FORCE=True)
-        if result == 0:
-            output.AnswerBuffer("SUCCESS: Pipeline initiated - you may close this page", 'text/plain')
-        elif result == 10:
-            output.AnswerBuffer("This series was not identified as a Urography series. If you are sure it is, please contact Fraser with examID and series number.", 'text/plain')
-        else:
-            output.AnswerBuffer("Oh no! Error encountered: please contact Fraser (fraser.callaghan@kispi.uzh.ch OR 3130)", 'text/plain')
-    else:
-        output.SendMethodNotAllowed('GET')
+# def ManualCallURO(output, uri, **request):
+#     if request['method'] == 'GET':
+#         # Retrieve the series ID from the regular expression (*)
+#         seriesId = request['groups'][0]
+#         result = AutoPipelineOnStableSeries(seriesId, FORCE=True)
+#         if result == 0:
+#             output.AnswerBuffer("SUCCESS: Pipeline initiated - you may close this page", 'text/plain')
+#         elif result == 10:
+#             output.AnswerBuffer("This series was not identified as a Urography series. If you are sure it is, please contact Fraser with examID and series number.", 'text/plain')
+#         else:
+#             output.AnswerBuffer("Oh no! Error encountered: please contact Fraser (fraser.callaghan@kispi.uzh.ch OR 3130)", 'text/plain')
+#     else:
+#         output.SendMethodNotAllowed('GET')
 
 def OnChange(changeType, level, resource):
-
-    # at startup, we should detect all series that are currently in
-    # Orthanc and we shall queue them "for processing"
     if changeType == orthanc.ChangeType.ORTHANC_STARTED:
-        allSeriesIds = json.loads(orthanc.RestApiGet("/series"))
-        for seriesId in allSeriesIds:
-            AutoPipelineOnStableSeries(seriesId, FORCE=False)
+        pass
+        # allSeriesIds = json.loads(orthanc.RestApiGet("/series"))
+        # for seriesId in allSeriesIds:
+        #     AutoPipelineOnStableSeries(seriesId, FORCE=False)
 
     elif changeType == orthanc.ChangeType.ORTHANC_STOPPED:
         pass
 
-    # everytime a new series is received, we shall check it then process
-    elif changeType == orthanc.ChangeType.STABLE_SERIES:
-        AutoPipelineOnStableSeries(resource, FORCE=True)
-        # Force true so that if paused and then restarted then will overwrite
+    # everytime a new study is received, we shall check it then process
+    elif changeType == orthanc.ChangeType.STABLE_STUDY:
+        try: 
+            AutoPipelineOnStableStudy(resource, FORCE=True)
+            # Force true so that if paused and then restarted then will overwrite
+        except Exception as e: # Catch all general exceptions and debug
+            logger.critical(e)
+
+    # # everytime a new series is received, we shall check it then process
+    # elif changeType == orthanc.ChangeType.STABLE_SERIES:
+    #     AutoPipelineOnStableSeries(resource, FORCE=True)
+    #     # Force true so that if paused and then restarted then will overwrite
 
 
 # ================================================================================================================
@@ -185,10 +305,10 @@ def OnChange(changeType, level, resource):
 # (1) Add button on series labelled "Send series to Urography pipeline"
 #        This button calls href: '/uropipeline/' + seriesID
 #        This href is linked back to function here: ManualCallURO
-orthanc.RegisterRestCallback('/uropipeline/(.*)', ManualCallURO)  # (*)
-# here add the actual button
-with open("/python/extend-explorer.js", "r") as f:
-    orthanc.ExtendOrthancExplorer(f.read())
+# orthanc.RegisterRestCallback('/uropipeline/(.*)', ManualCallURO)  # (*)
+# # here add the actual button
+# with open("/python/extend-explorer.js", "r") as f:
+#     orthanc.ExtendOrthancExplorer(f.read())
 
 # (2) Register a callback to the OnChange signal
 #       When ever a change occurs then "OnChange" function called
